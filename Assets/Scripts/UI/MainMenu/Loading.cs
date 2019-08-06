bce2fd9acd98cbfa5e738e88a154c7b356077334
Andrew <cryenginemail@gmail.com>
2019-08-06 23:54:11 +0400

    main menu

---
+++ Assets/Scripts/UI/MainMenu/Loading.cs
@@ -0,0 +1,59 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.MainMenu
+{
+    public class Loading : MonoBehaviour
+    {
+        private const float MAX_PROGRESS_SCALE = 1f;
+        
+        private AsyncOperation sceneLoadingOperation;
+        [SerializeField]
+        private RectTransform progressBar;
+        
+        private void Awake()
+        {
+            
+            
+            
+            
+        }
+
+
+        private void Update()
+        {
+            if (gameObject.activeSelf)
+            {
+                if (sceneLoadingOperation.isDone)
+                {
+                    Hide();
+                }
+                
+                SetProgress(sceneLoadingOperation.progress);
+            }
+        }
+
+        public void ShowAndLoad(int scene)
+        {
+            gameObject.SetActive(true);
+            sceneLoadingOperation = SceneManager.LoadSceneAsync(scene);
+            SetProgress(0f);
+//            sceneLoadingOperation.progress;
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+            sceneLoadingOperation = null;
+        }
+
+
+        void SetProgress(float progress)
+        {
+            progressBar.transform.localScale = new Vector3(progress, 1, 1);
+        }
+        
+    }
+}
